6f84042c713953914c677cce3a6739f6c2e56b77
Donal Anglin <donal.anglin@gmail.com>
2021-03-18 13:13:41 +0000

    Added Dockerfile and action to build and upload it

---
+++ src/main/java/com/anglind/github/Main.java
@@ -0,0 +1,10 @@
+package com.anglind.github;
+
+import com.anglind.github.actions.ClassForTest;
+
+public class Main {
+    public static void main(final String[] args) {
+        final ClassForTest classForTest = new ClassForTest();
+        System.out.println(classForTest.doSomething());
+    }
+}
